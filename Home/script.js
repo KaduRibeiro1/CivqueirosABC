4aa909cb08a223d1a5a0ae612a934de2f6b14d7c
KaduhRibeiro1 <carlos.ribeiro@sptech.school>
2024-11-20 22:56:08 -0300

    ajustes homepage

--- Home/script.js
+++ Home/script.js
@@ -1,13 +1,13 @@
 let currentImageIndex = 0;
 const images = [
-    'images/civic_armelin1.jpg',
-    'images/civic_armelin2.jpg',
-    'images/civic_thales1.jpg',
-    'images/civic_thales2.jpg',
-    'images/civic_thales3.jpg',
-    'images/civic_thales4.jpg',
-    'images/civic_mirella1.jpg',
-    'images/civic_gabi1.jpg'
+    '../images/civic_armelin1.jpg',
+    '../images/civic_armelin2.jpg',
+    '../images/civic_thales1.jpg',
+    '../images/civic_thales2.jpg',
+    '../images/civic_thales3.jpg',
+    '../images/civic_thales4.jpg',
+    '../images/civic_mirella1.jpg',
+    '../images/civic_gabi1.jpg'
 ];
 
 function updateImage() {
